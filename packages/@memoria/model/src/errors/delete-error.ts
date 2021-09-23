75d007be077649273d7b036139b852b255b86e89
Izel Nakri <contact@izelnakri.com>
2021-09-24 01:57:03 +0200

    simplify @memoria/model Error inits

--- packages/@memoria/model/src/errors/delete-error.ts
+++ packages/@memoria/model/src/errors/delete-error.ts
@@ -1,11 +1,10 @@
 import ChangesetError from "./changeset-error.js";
-import type Model from "../model.js";
 import type { ErrorMetadata } from "../error.js";
 import type Changeset from "../changeset.js";
 
 export default class DeleteError extends ChangesetError {
-  constructor(modelOrChangeset: Model | Changeset, errorMetadata: ErrorMetadata) {
-    super(modelOrChangeset, errorMetadata, "Memoria.DeleteError");
+  constructor(changeset: Changeset, errorMetadata: ErrorMetadata) {
+    super(changeset, errorMetadata, "Memoria.DeleteError");
 
     return this;
   }
