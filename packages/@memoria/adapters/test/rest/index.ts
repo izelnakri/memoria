4bc38ce4bc73aac646e95a9001b80c22cdd136b7
Izel Nakri <contact@izelnakri.com>
2021-09-27 04:24:49 +0200

    RESTAdapter add resetRecords to node tests

--- packages/@memoria/adapters/test/rest/index.ts
+++ packages/@memoria/adapters/test/rest/index.ts
@@ -4,4 +4,5 @@ import "./insert-test.js";
 import "./query-test.js";
 import "./relationship-id-test.js";
 import "./relationship-uuid-test.js";
+import "./reset-records-fixtures-test.js";
 import "./update-test.js";
