f95afc052255462366334b81458fa0f6dc3be0f2
Izel Nakri <contact@izelnakri.com>
2022-02-01 05:39:44 +0100

    fix sql belongs-to tests

--- packages/@memoria/adapters/test/sql/index.ts
+++ packages/@memoria/adapters/test/sql/index.ts
@@ -2,6 +2,8 @@ import "./default-attributes-test.js";
 import "./delete-test.js";
 import "./insert-test.js";
 import "./query-test.js";
+import "./relationships/belongs-to-id-test.js";
+import "./relationships/belongs-to-uuid-test.js";
 import "./serializer-embed-id-test.js";
 import "./serializer-embed-uuid-test.js";
 import "./reset-records-fixtures-test.js";
