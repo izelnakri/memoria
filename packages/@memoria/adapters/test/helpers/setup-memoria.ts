1d1fc0ad451c999d21d5d2578fad275d99d5c50a
Izel Nakri <contact@izelnakri.com>
2022-09-20 07:06:34 +0200

    assert.hasMany & assert.strictHasMany helpers

--- packages/@memoria/adapters/test/helpers/setup-memoria.ts
+++ packages/@memoria/adapters/test/helpers/setup-memoria.ts
@@ -1,6 +1,7 @@
 import match from "match-json";
 import QUnit from "qunitx";
 import Model, { Schema, DB } from "@memoria/model";
+import type { HasManyArray } from "@memoria/model";
 
 export default function (hooks) {
   hooks.before(function () {
@@ -12,6 +13,40 @@ export default function (hooks) {
         expected: expected,
         message,
       });
+    };
+    let assert = QUnit.assert;
+    QUnit.assert.hasMany = function (hasManyArray: HasManyArray, expectedHasManyArray: Model[], strictMode?: boolean) {
+      if (!Array.isArray(hasManyArray) || !Array.isArray(expectedHasManyArray)) {
+        return this.pushResult({
+          result: false,
+          actual: hasManyArray,
+          expected: expectedHasManyArray,
+          message: Array.isArray(hasManyArray)
+            ? "assert.hasMany(hasManyArray, expectedHasManyArray): expectedHasManyArray is not an array"
+            : "assert.hasMany(hasManyArray, expectedHasManyArray): hasManyArray is not an array",
+        });
+      }
+      let belongsToKey = hasManyArray.metadata.reverseRelationshipName;
+      let belongsToForeignKey = hasManyArray.metadata.reverseRelationshipForeignKeyColumnName;
+      let TargetModel = hasManyArray.belongsTo.constructor as typeof Model;
+      let targetPrimaryKeyName = TargetModel.primaryKeyName;
+
+      assert.deepEqual(hasManyArray, expectedHasManyArray);
+      expectedHasManyArray.forEach((expectedModel: Model, index: number) => {
+        assert.strictEqual(hasManyArray[index], expectedModel);
+
+        if (strictMode) {
+          assert.strictEqual(expectedModel[belongsToKey], hasManyArray.belongsTo);
+        } else {
+          let primaryKeyName = (expectedModel[belongsToKey].constructor as typeof Model).primaryKeyName;
+          assert.equal(expectedModel[belongsToKey][primaryKeyName], hasManyArray.belongsTo[targetPrimaryKeyName]);
+        }
+
+        assert.equal(expectedModel[belongsToForeignKey], hasManyArray.belongsTo[targetPrimaryKeyName]);
+      });
+    };
+    QUnit.assert.strictHasMany = function (hasManyArray: HasManyArray, expectedHasManyArray: Model[]) {
+      return QUnit.assert.hasMany(hasManyArray, expectedHasManyArray, true);
     };
   });
   hooks.beforeEach(async function () {
