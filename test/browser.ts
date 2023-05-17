1cdd6f79fbab0578386af9a5d54e8ceac94e3120
Izel Nakri <contact@izelnakri.com>
2023-05-17 06:34:58 +0200

    package.json adjusted to new test structure

--- test/browser.ts
+++ test/browser.ts
@@ -1,5 +1,5 @@
+import "../packages/@memoria/model/test/index.js";
 import "../packages/@memoria/adapters/test/memory/index.js";
 import "../packages/@memoria/adapters/test/rest/index.js";
-import "../packages/@memoria/model/test/index.js";
-import "../packages/@memoria/response/test/index.js";
 import "../packages/@memoria/server/test/index.js";
+import "../packages/@memoria/response/test/index.js";
--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,16 @@
-// TODO: make these work on node.js somehow through ts transpilation
-import "./browser.js";
+// import "./server.js";
+import "../packages/@memoria/model/test/index.js";
+import "../packages/@memoria/adapters/test/memory/index.js";
+import "../packages/@memoria/adapters/test/rest/index.js";
 import "../packages/@memoria/adapters/test/sql/index.js";
+
+// import "../packages/@memoria/server/src/set-pretender-context.js";
+// import "../packages/@memoria/server/test/index.js";
+// import "../packages/@memoria/response/test/index.js";
+
+// TODO: This type of mocking doesnt work currently it gives:
+// Error: AggregateError
+// at Object.dispatchError (/home/izelnakri/Github/memoria/node_modules/jsdom/lib/jsdom/living/xhr/xhr-utils.js:63:19)
+
+// import "../packages/@memoria/server/test/index.js";
+// import "../packages/@memoria/response/test/index.js";
